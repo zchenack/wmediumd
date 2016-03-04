c322eeab1516937db2ffcd3e5d734b178d8d410b
Luca Lemmo <luca@linux.com>
2016-03-04 19:51:32 +0100

    list: fix some coding style errors
    This patch fixes:
    
    list.h:82: ERROR: "foo * bar" should be "foo *bar"
    list.h:82: ERROR: "foo * bar" should be "foo *bar"
    
    Signed-off-by: Luca Lemmo <luca@linux.com>

--- wmediumd/list.h
+++ wmediumd/list.h
@@ -79,7 +79,7 @@ static inline void list_add_tail(struct list_head *new, struct list_head *head)
  * This is only for internal list manipulation where we know
  * the prev/next entries already!
  */
-static inline void __list_del(struct list_head * prev, struct list_head * next)
+static inline void __list_del(struct list_head *prev, struct list_head *next)
 {
 	next->prev = prev;
 	prev->next = next;
